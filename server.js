4720a38de0bb71422cd96b14975a9e12cb57fda1
Aleksandar Grbic <agrbicpp@gmail.com>
2017-04-14 16:44:15 +0200

    my first commit

---
+++ server.js
@@ -0,0 +1,9 @@
+const express = require('express');
+
+const app = express();
+
+app.get('/', (req, res) => {
+  res.status(200).send("Simple API with Heroku, Node & Travis!");
+});
+
+app.listen(process.env.PORT || 8080);
